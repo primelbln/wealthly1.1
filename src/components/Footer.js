22d4a593c7ea124329fd28ee095d8b675dcccd33
Felix Werner <kontakt@felix-werner.de>
2023-08-31 15:29:26 +0200

    colors added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-gray-400 text-center p-3 mt-5 text-white">
+      Made by L & F with{" "}
+      <FontAwesomeIcon style={{ color: "#ff0000" }} icon={faHeart} />
+    </div>
+  );
 };
 
 export default Footer;
