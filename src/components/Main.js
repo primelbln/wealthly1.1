c7ea261ecc12c66ef210f535bf3bfc97f4126e57
TechSavvyLeonard <leonard.kehl@gmail.com>
2023-09-04 10:57:44 +0200

    axios implemented via Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,27 @@ import {
   faDroplet,
   faCloudRain,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import axios from "axios";
 
-const Main = () => {
+function Main() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState(``);
+
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=50ed82754d4463602922bfb138532577`;
+
+const searchLocation = (event) => {
+  if (event.key === `Enter`) {
+
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data);
+    })
+  }
+}
+
+
   return (
     <>
       <h1 className="text-center p-2 mb-2.5 bg-flamingo-100 text-white">
@@ -18,6 +37,9 @@ const Main = () => {
       <div className="flex justify-center">
         <div className="rounded-md shadow-md w-1/2 text-center">
           <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress = {searchLocation}
             className="m-1.5 bg-gray-300 p-1.5 rounded-md shadow-md"
             type="text"
             placeholder="enter location..."
@@ -26,8 +48,9 @@ const Main = () => {
           <button>
             <FontAwesomeIcon className="" icon={faMagnifyingGlass} />
           </button>
+          <p>{data.name}</p>
           <img src="https://picsum.photos/200" alt="cloudy with sun" />
-          <p>33°C</p>
+          <p>{data.main.temp}°C</p>
           <p></p>
           <span>
             <FontAwesomeIcon icon={faDroplet} />
