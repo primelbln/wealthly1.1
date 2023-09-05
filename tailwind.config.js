c8c233372e4d330956dbe358d96d074a8b040bef
Felix Werner <kontakt@felix-werner.de>
2023-09-05 10:56:17 +0200

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    colors: {
-      tuna: {
-        100: "#3f3f41",
-        200: "#323234",
-        300: "#252527",
-      },
-      flamingo: {
-        100: "#d56d48",
-        200: "#d97b5a",
-        300: "#dd8a6c",
-      },
-      white: "#fff",
-    },
   },
   plugins: [],
 };
