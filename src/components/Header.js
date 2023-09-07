b60aaeb385854d862a9864907904c7aa476e695f
Felix Werner <kontakt@felix-werner.de>
2023-09-07 16:29:44 +0200

    some files deleted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import mainLogo from "../assets/logo.png";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+// changelog icon for later use
+// import { faSitemap } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
   return (
     <nav className="flex bg-slate-900 text-white justify-between items-center p-4 sticky top-0 z-10 drop-shadow-2xl">
@@ -10,7 +12,11 @@ const Header = () => {
         alt="Weathly Logo"
       />
       <span className="text-2xl font-bold ">Weathly</span>
-      <ul>
+      <ul className="flex justify-center item-center gap-6">
+        {/* Changelog icon
+         <li>
+          <FontAwesomeIcon icon={faSitemap} size="2xl" />
+        </li> */}
         <li>
           <a
             href="https://github.com/primelbln/wealthly1.1/"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      height: {
-        128: "32rem",
-        140: "40rem",
-        200: "60rem",
-      },
-      maxHeight: {
-        200: "60rem",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
